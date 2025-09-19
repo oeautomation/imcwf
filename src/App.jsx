a3c2643575e5414b58e170b36806591ca4cd156a
oeautomation <amila@hemnette.com.au>
2025-09-19 06:59:52 +0530

    gh pages router change

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,24 @@
-import React from 'react'
-import CustomFrequencyWireframe from './CustomFrequencyWireframe.jsx'
+import React from "react";
+import { HashRouter, Routes, Route, Link } from "react-router-dom";
+import CustomFrequencyWireframe from "./CustomFrequencyWireframe.jsx";
+
+function Home() {
+  return (
+    <div className="p-6 space-y-3">
+      <h1 className="text-xl font-semibold">Home</h1>
+      <p>Go to the wireframe demo:</p>
+      <Link className="underline text-indigo-600" to="/wireframe">Open Custom Frequency</Link>
+    </div>
+  );
+}
 
 export default function App() {
-  return <CustomFrequencyWireframe />
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/wireframe" element={<CustomFrequencyWireframe />} />
+      </Routes>
+    </HashRouter>
+  );
 }
